a78703e888721f4b427a745ad0e5715aefc4d095
Scheufler, Henning <henning.scheufler@dlr.de>
2022-11-21 19:30:22 +0100

    update stefan problem

--- run/benchmark/phaseChange/stefanProblem/runAll.sh
+++ run/benchmark/phaseChange/stefanProblem/runAll.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
-cd ${0%/*} || exit 1    # Run from this directory
+#!/bin/bash
+cd ${0%/*} || exit 1                        # Run from this directory
 
 python genCases.py
 
-cat Allrun | tr -d "&" > tmp
-mv tmp Allrun
-chmod u+x Allrun
+while read fname ; do
+  if [[ !("$fname" =~ "baseCase") ]]
+  then
+    ($fname) &
+  fi
+done < <(find Cases -name "Allrun")
 
-./Allrun
+# wait for all process to finish
+wait
 
-python getResults.py
+python getData.py
+python plotData.py
