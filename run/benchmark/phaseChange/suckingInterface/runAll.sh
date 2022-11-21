27f87529727f11ae0a99683021a7c6a0a319cba1
Scheufler, Henning <henning.scheufler@dlr.de>
2022-11-21 19:29:52 +0100

    update suckingInterface

---
+++ run/benchmark/phaseChange/suckingInterface/runAll.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ${0%/*} || exit 1                        # Run from this directory
+
+python genCases.py
+
+while read fname ; do
+  if [[ !("$fname" =~ "baseCase") ]]
+  then
+    ($fname) &
+  fi
+done < <(find Cases -name "Allrun")
+
+# wait for all process to finish
+wait
+
+python getData.py
+python plotData.py
